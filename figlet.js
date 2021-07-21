d2498f6a883f0dddb2f3679b99bb91a229cc2a52
0ngezwa-felele <ongezwa@codex.com>
2021-07-21 11:40:43 +0200

    fixed change

--- figlet.js
+++ figlet.js
@@ -25,3 +25,9 @@ figlet.text('Boo!', {
     console.log(data);
 });
 
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
--- greet_xola.js
+++
@@ -1,6 +0,0 @@
-const chalk = require('chalk');
-//import the greet module that is in the current folder
-const greet = require('./greet');
-
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
